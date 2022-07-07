2eb2803c21e9bb291c7ace049a18bb2c0a718abb
Jorge Kimura <kimurajorge2001@gmail.com>
2022-07-07 14:41:19 -0300

    feat: Criação e teste da requisição que não depende de autenticação

--- src/controllers/requisicao.controllers.js
+++ src/controllers/requisicao.controllers.js
@@ -1,16 +1,34 @@
 export default class Requisicao {
     static base_url = "https://blog-m2.herokuapp.com";
-    static token = ''
+    static token = localStorage.getItem('@kenzie-blog:token');
     static headers = {
         "Content-Type": "application/json",
         Authorization: `Bearer ${this.token}`
     };
 
-    static async cadastrarUsuario() {
+    static async cadastrarUsuario(newUserData) {
         return await fetch(`${this.base_url}/users/register`, {
             method: "POST",
             headers: this.headers,
-            body: JSON.stringify()
-        } )
+            body: JSON.stringify(newUserData),
+        })
+        .then((response) => response.json())
+        .catch((error) => console.log(error));
+    };
+
+    static async logarUsuario(userData) {
+        return await fetch(`${this.base_url}/users/login`, {
+          method: "POST",
+          headers: this.headers,
+          body: JSON.stringify(userData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                localStorage.removeItem('@kenzie-blog:id')
+                localStorage.setItem('@kenzie-blog:userId', data.userId);
+                localStorage.setItem('@kenzie-blog:token', data.token);
+                return data
+            })
+            .catch(error => console.log(error))
     }
 }
