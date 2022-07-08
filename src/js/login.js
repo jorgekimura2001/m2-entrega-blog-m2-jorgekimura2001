57eb73b32cdabbcdbaad7abdff3b384b89889f47
Jorge Kimura <kimurajorge2001@gmail.com>
2022-07-07 21:46:03 -0300

    feat: configuração do botao login - pegando id do user e gerando um token

--- src/js/login.js
+++ src/js/login.js
@@ -6,3 +6,27 @@ const mainCards = document.querySelector('.cards');
 const cardLogin = Cards.cardLogin()
 
 mainCards.append(cardLogin)
+
+const form = document.querySelector(".form__login");
+const [
+  inputUserEmail,
+  inputUserPassword,
+  btnLogin,
+] = form;
+
+function escutadorEvento() {
+  const pessoa = {};
+  btnLogin.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    pessoa["email"] = inputUserEmail.value;
+    pessoa["password"] = inputUserPassword.value;
+    await Requisicao.logarUsuario(pessoa);
+    resetar();
+  });
+}
+escutadorEvento();
+
+function resetar() {
+  inputUserEmail.value = "";
+  inputUserPassword.value = "";
+}
