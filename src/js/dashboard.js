605ddb7a23289bbdce8f561adb046daee9cdba4c
Jorge Kimura <kimurajorge2001@gmail.com>
2022-07-08 17:29:10 -0300

    feat: criação cards de post recentes

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,21 +1,5 @@
 import DashBoard from "./controllers/dashboard.controller.js";
 
 DashBoard.headerInfoUser()
+DashBoard.listarPostsRecentes()
 
-// postsRecentes.forEach(element => {
-//     const avatar = element.user.avatarUrl;
-//     const username = element.user.username;
-//     const post = element.content;
-//     const tratativa = element.createdAt.split('T').reverse().slice(1)[0].split('-').reverse()
-//     tratativa.forEach(elem => {
-//         console.log(elem)
-//     })
-        
-//         //.replace('T', '-').replace('Z', '').split('-').reverse()
-//         //.splice(-3, 3)
-//     const dataTratada = Number(tratativa)
-//     const data = dataTratada.toString()
-//     console.log(tratativa);
-    
-//     const cardPostRecente = ComponentesDOM.cardPostsRecentes(avatar, username, post, )
-// });
